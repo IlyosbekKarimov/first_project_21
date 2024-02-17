d1514690a73a0da751e0756f9244f24f5c93183a
Ilyosbek <skeevesp@student.21-school.ru>
2024-02-17 11:23:09 +0500

    Modified

--- src/ai_door_control.sh
+++ src/ai_door_control.sh
@@ -3,13 +3,13 @@
 while (true) 
 do
     index=$[ $index + 1 ]
-    echo 'Обход!'
-    echo 'Идет проверка дверей ...'
-    echo 'Проверка степени закрытости двери #'$[ $index % 16 ]'. Секретный код доступа: '$[ RANDOM % $index ]'.'
-    echo 'Степень закрытости: 100.00%.'
+    echo 'Bypass!'
+    echo 'Doors are being checked...'
+    echo 'Checking the degree of door closure #'$[ $index % 16 ]'. Secret access code: '$[ RANDOM % $index ]'.'
+    echo 'Degree of closure: 100.00%.'
     if [ $[ RANDOM % 3 ] -eq 0 ]; then
        echo
-       echo 'Дополнительная проверка.'
+       echo 'additional verification.'
        echo '1...'
        echo '2...'
        sleep 1
@@ -18,7 +18,7 @@ do
        echo '4...'
        sleep 2
        echo '5...'
-       echo 'Проверка успешно завершена.'
+       echo 'Verification completed successfully.'
     fi
     sleep $[ RANDOM % 10 + 1 ];
 done;
